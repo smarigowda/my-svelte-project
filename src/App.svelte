fc2435e16e53aaf037fafb9ab026d24fbd8b0e96
Santosh Marigowda <santosharakere@gmail.com>
2019-07-11 21:36:57 +0100

    dynamic update

--- src/App.svelte
+++ src/App.svelte
@@ -1,18 +1,28 @@
 <script>
-	export let name;
-	export let age;
+  export let name;
+  export let age;
+  function incAge() {
+    age++;
+  }
+  const decAge = () => age--;
 </script>
 
 <style>
-	h1 {
-		display: block;
-		color: purple;
-		font-weight: 200;
-		margin: 0 auto;
-		margin-top: 50px;
-		text-align: center;
-	}
+  h1 {
+    display: block;
+    color: purple;
+    font-weight: 200;
+    margin-top: 50px;
+  }
+  .container {
+    margin: 0 auto;
+    text-align: center;
+  }
 </style>
 
-<h1>Hello... {name}, {age}!</h1>
-<h1>Welcome to Svelte...!</h1>
+<div class="container">
+  <h1>Hello... {name}, {age}!</h1>
+  <h1>Welcome to Svelte...!</h1>
+  <button on:click={incAge}>Increase</button>
+  <button on:click={decAge}>Decrease</button>
+</div>
