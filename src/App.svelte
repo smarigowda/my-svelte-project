257e8636e9762c347042be1ef0a82a46e84d5572
Santosh Marigowda <santosharakere@gmail.com>
2019-07-20 21:26:15 +0100

    SUC-7 reject function to reject id from meetups

--- src/App.svelte
+++ src/App.svelte
@@ -3,6 +3,13 @@
   import MeetupItem from "./Meetups/MeetupItem.svelte";
   import MeetupGrid from "./Meetups/MeetupGrid.svelte";
 
+  function reject(obj, keys) {
+    return Object.keys(obj)
+      .filter(k => !keys.includes(k))
+      .map(k => Object.assign({}, { [k]: obj[k] }))
+      .reduce((res, o) => Object.assign(res, o), {});
+  }
+
   let meetups = [
     {
       id: "m1",
@@ -23,10 +30,20 @@
         "In this session we will have some experts who will be teachig how to Swim",
       imageUrl:
         "https://s3-us-west-1.amazonaws.com/swimstrongfoundation/wp-content/uploads/2017/10/12141903/freestyle.jpg",
-      address: "27, Nerd Road, 34567 New Yark",
+      address: "14, Fulmer Close, 34567 New Yark",
       email: "swim@test.com"
     }
   ];
+
+  meetups = meetups.map(d => reject(d, ["id"]));
+
+  let title;
+
+  $: console.log(title);
+
+  function addMeetup() {
+    console.log("adding meetup to meetups array of objects...");
+  }
 </script>
 
 <style>
@@ -37,11 +54,32 @@
 
 <Header />
 <main id="meetups">
-  <form action="">
+  <form on:submit|preventDefault={addMeetup}>
     <div class="form-control">
       <label for="title">Title</label>
-      <input type="text" id="title">
+      <input type="text" id="title" bind:value={title} />
     </div>
+    <div class="form-control">
+      <label for="subtitle">Subtitle</label>
+      <input type="text" id="subtitle" />
+    </div>
+    <div class="form-control">
+      <label for="address">Address</label>
+      <input type="text" id="address" />
+    </div>
+    <div class="form-control">
+      <label for="imageurl">Image URL</label>
+      <input type="text" id="imageurl" />
+    </div>
+    <div class="form-control">
+      <label for="email">E-Mail</label>
+      <input type="email" id="email" />
+    </div>
+    <div class="form-control">
+      <label for="description">Description</label>
+      <textarea rows="3" type="text" id="description" />
+    </div>
+    <button type="submit">Save</button>
   </form>
   <MeetupGrid {meetups} />
 </main>
