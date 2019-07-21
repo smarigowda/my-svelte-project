a2d089b921e85e72f16a2fde6715c97acc46b2a9
Santosh Marigowda <santosharakere@gmail.com>
2019-07-21 15:41:33 +0100

    SUC-11 using Custom Events to communicate from child comp to parent comp

---
+++ src/Product.svelte
@@ -0,0 +1,21 @@
+<script>
+  import { createEventDispatcher } from "svelte";
+  export let productTitle;
+  const dispatch = createEventDispatcher();
+</script>
+
+<article>
+  <h1>{productTitle}</h1>
+  <button
+    on:click={() => {
+      dispatch('add-to-cart');
+    }}>
+    Add To Cart
+  </button>
+  <button
+    on:click={() => {
+      dispatch('delete');
+    }}>
+    Delete
+  </button>
+</article>
