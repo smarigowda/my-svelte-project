29733957ac25e9700f9afa3d53c3e435f8cc06af
Santosh Marigowda <santosharakere@gmail.com>
2019-07-21 21:43:29 +0100

    SUC-13 demo of Slots, Custom Events using a Modal Component

--- src/App.svelte
+++ src/App.svelte
@@ -1,7 +1,10 @@
 <script>
   import Product from "./Product.svelte";
+  import Modal from "./Modal.svelte";
   export let name;
   export let age;
+  let showModal = false;
+
   let productTitle = "A Book";
   function addToCart(event) {
     console.log(event);
@@ -19,8 +22,15 @@
 </script>
 
 {#each products as product}
-  <Product
-    {...product}
-    on:add-to-cart={addToCart}
-    on:delete={removeFromCart} />
+  <Product {...product} on:add-to-cart={addToCart} on:delete={removeFromCart} />
 {/each}
+
+<button on:click={() => (showModal = true)}>Show Modal</button>
+
+{#if showModal}
+  <Modal on:cancel={() => (showModal = false)}>
+    <h1 slot="header">Hello !</h1>
+    <h2>This works !!!</h2>
+    <button on:click={() => (showModal = false)}>Confirm</button>
+  </Modal>
+{/if}
