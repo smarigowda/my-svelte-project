7d87ff5cfdc81d4b668aef0a8b1295e2b901c1d0
Santosh Marigowda <santosharakere@gmail.com>
2019-07-21 16:08:14 +0100

    SUC-12 extracting event data, spreading props, default prop

--- src/App.svelte
+++ src/App.svelte
@@ -3,11 +3,24 @@
   export let name;
   export let age;
   let productTitle = "A Book";
+  function addToCart(event) {
+    console.log(event);
+  }
+  function removeFromCart(event) {
+    console.log(event);
+  }
+  const products = [
+    {
+      id: "676-987-654-876",
+      title: "A Book",
+      price: 9.99
+    }
+  ];
 </script>
 
-<Product
-  {productTitle}
-  on:add-to-cart={() => {
-    alert('add to cart button clicked...  ');
-  }}
-  on:delete={() => alert('delete button clicked...')} />
+{#each products as product}
+  <Product
+    {...product}
+    on:add-to-cart={addToCart}
+    on:delete={removeFromCart} />
+{/each}
--- src/Product.svelte
+++ src/Product.svelte
@@ -1,20 +1,27 @@
 <script>
   import { createEventDispatcher } from "svelte";
-  export let productTitle;
+  export let title;
+  export let price;
+//   export let bestseller = false;
+  export let bestseller = true;
   const dispatch = createEventDispatcher();
 </script>
 
 <article>
-  <h1>{productTitle}</h1>
+  <h1>{title}</h1>
+  <p>${price}</p>
+  {#if bestseller}
+  <h1>BEST SELLER ICON</h1>
+  {/if}
   <button
     on:click={() => {
-      dispatch('add-to-cart');
+      dispatch('add-to-cart', { id: '789-987-987' });
     }}>
     Add To Cart
   </button>
   <button
     on:click={() => {
-      dispatch('delete');
+      dispatch('delete', { id: '787-565-987' });
     }}>
     Delete
   </button>
