a5752c658a6c37bea5f832d19549d14e56097159
Santosh Marigowda <santosharakere@gmail.com>
2019-07-28 07:14:27 +0100

    SUC-24 binding to a single checkbox

--- src/App.svelte
+++ src/App.svelte
@@ -1,12 +1,14 @@
 <script>
   import CustomInput from "./CustomInput.svelte";
-  import Toggle from './Toggle.svelte';
+  import Toggle from "./Toggle.svelte";
   let value = "";
   let number;
   let selectedOption = 2;
+  let orangeChecked = true;
   $: console.log("value = ", value);
   $: console.log("number =", number);
-  $: console.log("selectedOption =", selectedOption)
+  $: console.log("selectedOption =", selectedOption);
+  $: console.log("orangeChecked =", orangeChecked);
 </script>
 
 <style lang="scss">
@@ -22,9 +24,15 @@
   <!-- <input type="text" bind:value /> -->
   <hr />
   <label for="custominput">Custom Input</label>
-  <CustomInput bind:value/>
-  <Toggle bind:choosenOption={selectedOption}/>
-  <hr>
+  <CustomInput bind:value />
+  <Toggle bind:choosenOption={selectedOption} />
+  <hr />
   <label for="">Number Input</label>
-  <input type="number" bind:value={number}>
+  <input type="number" bind:value={number} />
+  <hr />
+  <h1>Checkbox</h1>
+  <label for="">
+    <input type="checkbox" bind:checked={orangeChecked}/>
+    Check Orange
+  </label>
 </div>
