5957280bb6cbc3440f6d5ece0a93596db874d4cc
Santosh Marigowda <santosharakere@gmail.com>
2019-07-28 06:25:07 +0100

    SUC-24 simple input of type text

--- src/App.svelte
+++ src/App.svelte
@@ -1,9 +1,19 @@
 <script>
+  let value = '';
+  $: console.log('value = ', value);
 </script>
 
-<style>
+<style lang="scss">
+.container {
+  & > input {
+    outline: none;
+  }
+}
+
+
 </style>
 
 <div class="container">
   <h1>Hello...</h1>
+  <input type="text" bind:value />
 </div>
