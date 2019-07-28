0f7a2cb83b96a7e57aae6c6530e0871b662c6d1b
Santosh Marigowda <santosharakere@gmail.com>
2019-07-28 06:35:18 +0100

    SUC-24 binding to CustomInput

--- src/App.svelte
+++ src/App.svelte
@@ -1,19 +1,21 @@
 <script>
-  let value = '';
-  $: console.log('value = ', value);
+  import CustomInput from "./CustomInput.svelte";
+  let value = "";
+  $: console.log("value = ", value);
 </script>
 
 <style lang="scss">
-.container {
-  & > input {
-    outline: none;
+  .container {
+    & > input {
+      outline: none;
+    }
   }
-}
-
-
 </style>
 
 <div class="container">
   <h1>Hello...</h1>
-  <input type="text" bind:value />
+  <!-- <input type="text" bind:value /> -->
+  <hr />
+  <label for="custominput">Custom Input</label>
+  <CustomInput bind:value/>
 </div>
