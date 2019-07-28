75c39815020923f390382c99b6c978fb7b0e0abe
Santosh Marigowda <santosharakere@gmail.com>
2019-07-28 06:19:13 +0100

    SUC-24 cleanup

--- src/App.svelte
+++ src/App.svelte
@@ -1,77 +1,9 @@
 <script>
-  import ContactCard from "./ContactCard.svelte";
-  import CourseGoal from './CourseGoal.svelte';
-  export let name;
-  export let age;
-  export let upperName;
-
-
-  let jobTitle = "Software Developer";
-  let jobDescription = "Develop Automated Tests using JS Libraries";
-  let htmlText;
-  let courseGoal;
-
-  function incAge() {
-    age++;
-  }
-  const decAge = () => age--;
-  $: upperName = name.toUpperCase();
-
-  const changeName = () => {
-    name = "Roopa Santosh";
-  };
-  const nameInput = event => {
-    name = event.target.value;
-  };
 </script>
 
 <style>
-  h1 {
-    display: block;
-    color: blueviolet;
-    font-weight: 200;
-    margin-top: 50px;
-  }
-  .container {
-    margin: 0 auto;
-    text-align: center;
-  }
-  button {
-    outline: none;
-    border: none;
-    padding: 13px;
-    padding-left: 15px;
-    padding-right: 15px;
-    border-radius: 5px;
-    color: white;
-    background-color: blueviolet;
-    font-weight: 100;
-    cursor: pointer;
-    border: 1px solid blueviolet;
-  }
-  button:hover {
-    background-color: white;
-    color: blueviolet;
-    border: 1px solid blueviolet;
-  }
 </style>
 
 <div class="container">
-  <h1>Hello... {name.toUpperCase()}, {age}!</h1>
-  <h1>Reactive... {upperName}, {age}!</h1>
-  <h1>Welcome to Svelte...!</h1>
-  <button on:click={incAge}>Increase</button>
-  <button on:click={decAge}>Decrease</button>
-
-  <button on:click={changeName}>Change Name</button>
-  <hr />
-
-  <textarea name="html" id="" cols="30" rows="10" bind:value={htmlText} />
-  <p>{@html htmlText}</p>
-  <hr />
-  <label for="name">Change Name:</label>
-  <input type="text" value={name} on:input={nameInput} />
-  <!-- <input type="text" bind:value={name} /> -->
-  <ContactCard userName={name} {jobTitle} {jobDescription} />
-  <CourseGoal courseGoal="Develop Svelte Apps"/>
+  <h1>Hello...</h1>
 </div>
