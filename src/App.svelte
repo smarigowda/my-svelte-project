457516eff3ad8314006d7f463b760340a98d1269
Santosh Marigowda <santosharakere@gmail.com>
2019-08-04 16:26:07 +0100

    SUC-39 meetup data in meetup store

--- src/App.svelte
+++ src/App.svelte
@@ -6,6 +6,7 @@
   import Input from "./UI/Input.svelte";
   import Button from "./UI/Button.svelte";
   import Modal from "./UI/Modal.svelte";
+  import { meetupsStore } from "./Meetups/meetups-store.js";
 
   // function reject(obj, keys) {
   //   return Object.assign(
@@ -16,34 +17,12 @@
   //   );
   // }
 
-  let meetups = [
-    {
-      id: "m1",
-      title: "Coding Bootcamp",
-      subtitle: "Learn to code in 2 hours",
-      description:
-        "In this meetup we will have some experts who will be teachig how to code",
-      imageUrl:
-        "https://insights.dice.com/wp-content/uploads/2019/05/Coding-Bootcamp-Ratings-Dice.png",
-      address: "27, Nerd Road, 34567 New Yark",
-      email: "code@test.com",
-      isFavourite: false
-    },
-    {
-      id: "m2",
-      title: "Swimming Bootcamp",
-      subtitle: "Learn to swim in 2 months",
-      description:
-        "In this session we will have some experts who will be teachig how to Swim",
-      imageUrl:
-        "https://s3-us-west-1.amazonaws.com/swimstrongfoundation/wp-content/uploads/2017/10/12141903/freestyle.jpg",
-      address: "14, Fulmer Close, 34567 New Yark",
-      email: "swim@test.com",
-      isFavourite: false
-    }
-  ];
+  let meetups = [];
 
-  // $: meetups = meetups.map(d => reject(d, ["id"]));
+  meetupsStore.subscribe(data => {
+    meetups = data;
+    console.log("meetups", meetups);
+  });
 
   let title = "";
   let subtitle = "";
@@ -102,7 +81,9 @@
     </div>
   {/if}
   {#if editMeetup}
-    <EditMeetup on:addmeetup={addMeetup} on:cancelmodal={() => editMeetup = false}/>
+    <EditMeetup
+      on:addmeetup={addMeetup}
+      on:cancelmodal={() => (editMeetup = false)} />
   {/if}
   <MeetupGrid {meetups} on:togglefavourite={handleToggleFavourite} />
 </main>
---
+++ src/Meetups/meetups-store.js
@@ -0,0 +1,30 @@
+import { writable } from "svelte/store";
+
+let meetupsList = [
+  {
+    id: "m1",
+    title: "Coding Bootcamp",
+    subtitle: "Learn to code in 2 hours",
+    description:
+      "In this meetup we will have some experts who will be teachig how to code",
+    imageUrl:
+      "https://insights.dice.com/wp-content/uploads/2019/05/Coding-Bootcamp-Ratings-Dice.png",
+    address: "27, Nerd Road, 34567 New Yark",
+    email: "code@test.com",
+    isFavourite: false
+  },
+  {
+    id: "m2",
+    title: "Swimming Bootcamp",
+    subtitle: "Learn to swim in 2 months",
+    description:
+      "In this session we will have some experts who will be teachig how to Swim",
+    imageUrl:
+      "https://s3-us-west-1.amazonaws.com/swimstrongfoundation/wp-content/uploads/2017/10/12141903/freestyle.jpg",
+    address: "14, Fulmer Close, 34567 New Yark",
+    email: "swim@test.com",
+    isFavourite: false
+  }
+];
+
+export const meetupsStore = writable(meetupsList);
