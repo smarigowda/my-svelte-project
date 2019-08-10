d25f1c79bb90bfef25689a8ef5703cc61bc635a2
Santosh Marigowda <santosharakere@gmail.com>
2019-08-10 10:36:48 +0100

    SUC-53 initial project setup

--- src/main.js
+++ src/main.js
@@ -1,11 +1,7 @@
-import App from './App.svelte';
+import App from "./App.svelte";
 
 const app = new App({
-	target: document.body,
-	props: {
-		name: 'Santosh Marigowda',
-		age: 2019 - 1974
-	}
+  target: document.body
 });
 
 export default app;
