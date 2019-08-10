4ae618be34a3610b2b0fefcb8b179f0ce281c335
Santosh Marigowda <santosharakere@gmail.com>
2019-08-10 11:46:20 +0100

    SUC-47 demo of tweened store

--- src/App.svelte
+++ src/App.svelte
@@ -1,2 +1,17 @@
 <script>
+  import { writable } from "svelte/store";
+  import { tweened } from "svelte/motion";
+  import { cubicIn } from "svelte/easing";
+
+  const progress = tweened(0, {
+    delay: 2000,
+    duration: 700,
+    easing: cubicIn
+  });
+
+  setTimeout(() => {
+    $progress = 0.5;
+  }, 0);
 </script>
+
+<progress value={$progress} />
